ae8d660972094383196bcb3ceb1f6a9e3322130f
Vanderstuck <niels@stuckyfamily.de>
2023-06-05 10:17:21 +0200

    fix overlay opening/closing

--- script/gametest.js
+++ script/gametest.js
@@ -1,6 +1,3 @@
-const map = document.getElementById("map1");
-const coord = map.getAttribute("coords");
-
 function startTimer(duration, display) {
   var timer = duration,
     minutes,
@@ -21,8 +18,36 @@ function startTimer(duration, display) {
   }, 1000);
 }
 
-window.onload = function () {
-  var fiveMinutes = 5 * 60,
-    display = document.querySelector("#timer");
-  startTimer(fiveMinutes, display);
-};
+function init() {
+  var gameItems = document.getElementsByClassName("game-image");
+  var overlay = document.getElementById("overlay");
+  var closeOverlay = document.getElementById("closeOverlay");
+  for (var i = 0; i < gameItems.length; i++) {
+      gameItems[i].addEventListener("click", function () {
+        addOverlay();
+      });
+  }
+  closeOverlay.addEventListener("click", function () {
+    removeOverlay();
+  });
+}
+
+function addOverlay() {
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "block";
+}
+
+function removeOverlay() {
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "none";
+}
+
+//window.onload = function () {
+//  var fiveMinutes = 5 * 60,
+//    display = document.querySelector("#timer");
+//  startTimer(fiveMinutes, display);
+//};
+
+window.onload = init;
+
+
